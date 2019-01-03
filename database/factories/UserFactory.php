d4b4d0daaf740ca0aa6f12016c701ec2ca2d8e10
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2019-01-03 15:00:37 -0600

    change password default user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2a$04$nYH8j1ZUaTF4A0vnRYdcKe8sJHzSsW/Pqpj0GqlCkd3C7XzBSRAVe', // secret
         'remember_token' => str_random(10),
     ];
 });
