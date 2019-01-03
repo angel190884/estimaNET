1a68e5c132f4cb56c995c1623d1d230f41831b1a
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2019-01-03 15:36:49 -0600

    add model Company

---
+++ app/Company.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'rfc', 'reason_social', 'admin_unique', 'telephone', 'address', 'logo', 'background', 'interbank_key', 'bank_account',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
