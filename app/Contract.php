65825ad53f41c1eb1b2cf8302c634c90367eaa80
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2019-01-03 15:41:56 -0600

    add relation contract->users and attributes

--- app/Contract.php
+++ app/Contract.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Contract extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
+
+    public function getCodeOkAttribute()
+    {
+        return strtoupper($this->code);
+    }
+
+    public function getTotalOkAttribute()
+    {
+        return number_format($this->total, 2, '.', ',');
+    }
+
+    public function getStartOkAttribute()
+    {
+        return Carbon::parse($this->start)->format('d-m-Y');
+    }
 }
