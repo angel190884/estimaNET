a38dc11dcc749af32e91e1502aa38cdef923f264
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2019-01-10 10:30:00 -0600

    add route logs

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 
 Auth::routes(['verify' => true]);
 
