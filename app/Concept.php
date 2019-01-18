b264da3ee7d0bbef2095e071709eaca6b9357397
Angel Daniel Peregrino Juarez <angel190884@gmail.com>
2019-01-17 18:04:30 -0600

    add model and controller the concept

---
+++ app/Concept.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Concept extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'contract_id',
+        'code_concept',
+        'location',
+        'address',
+        'name',
+        'measurement_unit',
+        'quantity',
+        'unit_price',
+        'type'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+
+    ];
+
+    /**
+     * Relations.
+     */
+    public function contract()
+    {
+        return $this->belongsTo(Contract::class);
+    }
+}
